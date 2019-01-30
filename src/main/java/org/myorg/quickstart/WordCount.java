bd0421ba25a8616ed5e787d0d3b3257f1e33f556
root <957062024@qq.com>
2019-01-30 16:13:35 +0800

    study flink

---
+++ src/main/java/org/myorg/quickstart/WordCount.java
@@ -0,0 +1,41 @@
+package org.myorg.quickstart;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSet<String> text = env.fromElements(
+          "To be, or not to be,--thah is then question;--",
+          "whether 'tis nobler in then mind to suffer",
+          "The slings and arrows of outrageous fortune",
+          "Or to take arms against a sea of troubles,"
+        );
+
+        DataSet<Tuple2<String, Integer>> counts =
+                text.flatMap(new LineSplitter())
+                .groupBy(0)
+                .sum(1);
+
+        counts.print();
+    }
+
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            for (String token : tokens){
+                if(token.length() > 0){
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+}
